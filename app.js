c193eef4e9d4ff41c4fdd5d3263c00cf9097dc3a
Nathi33 <nathi33.drnt@outlook.com>
2025-02-11 16:20:23 +0100

    add init_project

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const mongodb = require("./db/mongo");
+
+// Initialisation de la connexion à la base de données MongoDB
+mongodb.initClientDbConnection();
 
 const app = express();
 
+// Configuration des CORS en acceptant toutes les origines et on récupère le token d'authentification côté client
+app.use(
+  cors({
+    exposedHeaders: ["Authorization"],
+    origin: "*",
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
+// Configuration de la route principale avec pour url de base "/"
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
+// Retour en cas de requête sur une route inexistante
+app.use(function (req, res, next) {
+  res
+    .status(404)
+    .json({
+      name: "API_RUSSELL",
+      version: "1.0",
+      status: 404,
+      message: "Not Found",
+    });
+});
 
 module.exports = app;
