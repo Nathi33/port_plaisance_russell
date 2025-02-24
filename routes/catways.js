40ddfd38f2d1d3d423d0ab2d313e09ec33e9cb9d
Nathi33 <nathi33.drnt@outlook.com>
2025-02-24 13:43:16 +0100

    add private_route

--- routes/catways.js
+++ routes/catways.js
@@ -5,22 +5,14 @@ const catwayService = require("../services/catways");
 
 const auth = require("../middlewares/auth");
 
-// Route pour lire les informations de tous les catways
-router.get("/", catwayService.getAll);
+// Routes protégées avec JWT
+router.get("/", auth, catwayService.getAll); // Route pour lire les informations de tous les catways
+router.post("/add", auth, catwayService.add); // Route pour ajouter un catway
+router.delete("/:id", auth, catwayService.delete); // Route pour supprimer un catway
 
-// Route pour lire les informations d'un catway
-router.get("/:id", catwayService.getById);
-
-// Route pour ajouter un catway
-router.post("/add", auth, catwayService.add);
-
-// Route pour mettre à jour un catway
-router.put("/update_catway/:id", catwayService.update);
-
-// Route pour mettre à jour partiellement un catway
-router.patch("/:id", catwayService.partialUpdate);
-
-// Route pour supprimer un catway
-router.delete("/:id", auth, catwayService.delete);
+// Routes publiques
+router.get("/:id", catwayService.getById); // Route pour lire les informations d'un catway
+router.put("/update_catway/:id", catwayService.update); // Route pour mettre à jour un catway
+router.patch("/update_catway/:id", catwayService.partialUpdate); // Route pour mettre à jour partiellement un catway
 
 module.exports = router;
